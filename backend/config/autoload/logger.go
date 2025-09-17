ac0dfbef6bf9ce3971b8acc42a415488ee6d1cdc
mac-haley <hongwei.huang42@gmail.com>
2025-09-17 23:05:01 +0800

    complete logger code

--- backend/config/autoload/logger.go
+++ backend/config/autoload/logger.go
@@ -1,5 +1,19 @@
 package autoload
 
 type LoggerConfig struct {
-	DefaultDivision string `mapstructure:"default_division"`
+	DefaultDivision string       `mapstructure:"default_division"`
+	DivisionTime    DivisionTime `mapstructure:"division_time"`
+	DivisionSize    DivisionSize `mapstructure:"division_size"`
+}
+
+type DivisionTime struct {
+	MaxAge       int64 `mapstructure:"max_age"`
+	RotationTime int64 `mapstructure:"rotation_time"`
+}
+
+type DivisionSize struct {
+	MaxSize    int64 `mapstructure:"max_size"`
+	MaxBackups int64 `mapstructure:"max_backups"`
+	MaxAge     int64 `mapstructure:"max_age"`
+	Compress   bool  `mapstructure:"compress"`
 }
--- backend/internal/pkg/logger/logger.go
+++ backend/internal/pkg/logger/logger.go
@@ -1,7 +1,10 @@
 package logger
 
 import (
+	rotatelogs "github.com/lestrrat-go/file-rotatelogs"
+	"github.com/natefinch/lumberjack/v2"
 	"insight/config"
+	"io"
 	"os"
 	"path/filepath"
 	"sync"
@@ -44,13 +47,40 @@ func createZapLog() *zap.Logger {
 	var writer zapcore.WriteSyncer
 	if Config.Logger.DefaultDivision == "size" {
 		// 按文件大小切割日志
-		writer = zapcore.AddSync(getLumberJackWriter(filename))
+		writer = zapcore.AddSync(getLumberJackWriter(filename, *Config))
 	} else {
 		// 按天切割日志
-		writer = zapcore.AddSync(getRotateWriter(filename))
+		writer = zapcore.AddSync(getRotateWriter(filename, *Config))
 	}
 	zapCore := zapcore.NewCore(encoder, writer, zap.InfoLevel)
 	//zap.AddStacktrace(zap.WarnLevel)
 	return zap.New(zapCore, zap.AddCaller())
+}
 
+// getRotateWriter 按日期切割日志
+func getRotateWriter(filename string, config config.Config) io.Writer {
+	maxAge := time.Duration(config.Logger.DivisionTime.MaxAge)
+	rotationTime := time.Duration(config.Logger.DivisionTime.RotationTime)
+	hook, err := rotatelogs.New(
+		filename+".%Y%m%d",
+		rotatelogs.WithLinkName(filename),
+		rotatelogs.WithMaxAge(time.Hour*24*maxAge),
+		rotatelogs.WithRotationTime(time.Hour*rotationTime), // 默认一天
+	)
+	if err != nil {
+		panic(err)
+	}
+	return hook
+}
+
+// getLumberJackWriter 按文件切割日志
+func getLumberJackWriter(filename string, config config.Config) io.Writer {
+	// 日志切割配置
+	return &lumberjac.Logger{
+		Filename:   filename,                              // 日志文件位置
+		MaxSize:    config.Logger.DivisionSize.MaxSize,    // 在进行切割之前，日志文件的最大大小（以MB为单位）
+		MaxBackups: config.Logger.DivisionSize.MaxBackups, // 保留旧文件的最大个数
+		MaxAge:     config.Logger.DivisionSize.MaxAge,     // 保留旧文件的最大天数
+		Compress:   config.Logger.DivisionSize.Compress,   // 是否压缩/归档旧文件
+	}
 }
