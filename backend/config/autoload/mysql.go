81b9ce55b72944e01f285110b1753e661b62bfa9
haley <p-huanghw@xiaopeng.com>
2025-09-30 15:23:05 +0800

    modify the database implementation

--- backend/config/autoload/mysql.go
+++ backend/config/autoload/mysql.go
@@ -1,9 +1,16 @@
 package autoload
 
 type MySQLConfig struct {
-	Host     string `mapstructure:"host"`
-	Port     int    `mapstructure:"port"`
-	Username string `mapstructure:"username"`
-	Password string `mapstructure:"password"`
-	Database string `mapstructure:"database"`
+	Host         string `mapstructure:"host"`
+	Port         int    `mapstructure:"port"`
+	Username     string `mapstructure:"username"`
+	Password     string `mapstructure:"password"`
+	Database     string `mapstructure:"database"`
+	PrintSql     bool   `mapstructure:"print_sql"`
+	LogLevel     string `mapstructure:"log_level"`
+	TablePrefix  string `mapstructure:"table_prefix"`
+	MaxIdleConns int    `mapstructure:"max_idle_conns"`
+	MaxOpenConns int    `mapstructure:"max_open_conns"`
+	MaxLifetime  int    `mapstructure:"max_life_time"`
+	Enable       bool   `mapstructure:"enable"`
 }
