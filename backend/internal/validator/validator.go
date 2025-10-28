bf51500e00ed671af92e4670f981aa516bc8aa6d
haley <p-huanghw@xiaopeng.com>
2025-10-28 19:58:14 +0800

    add valiate code

--- backend/internal/validator/validator.go
+++ backend/internal/validator/validator.go
@@ -6,11 +6,15 @@ import (
 	"sync"
 
 	"github.com/gin-gonic/gin/binding"
+	"github.com/go-playground/locales/en"
+	"github.com/go-playground/locales/zh"
 	ut "github.com/go-playground/universal-translator"
 	"github.com/go-playground/validator/v10"
+	enTranslations "github.com/go-playground/validator/v10/translations/en"
+	zhTranslations "github.com/go-playground/validator/v10/translations/zh"
 )
 
-var trans ut.Translator  // 全局校验器
+var trans ut.Translator // 全局校验器
 
 var once sync.Once
 
@@ -29,10 +33,50 @@ func validatorTrans(locale string) {
 	registerValidation()
 	// 注册一个获取json tag的自定义方法
 	validate.RegisterTagNameFunc(func(field reflect.StructField) string {
+		label := field.Tag.Get("label")
+		if label == "" {
+			label = field.Tag.Get("json")
+			if label == "" {
+				label = field.Tag.Get("form")
+			}
+		}
 
+		if label == "-" {
+			return ""
+		}
 
+		if label == "" {
+			return field.Name
+		}
+
+		return label
+	})
+
+	zhT := zh.New() // 中文翻译器
+	enT := en.New() // 英文翻译器
+	uni := ut.New(enT, zhT, enT)
+
+	trans, ok = uni.GetTranslator(locale)
+	if !ok {
+		panic("Initialize a language not supported by the validator")
+	}
+
+	var err error
+
+	switch locale {
+	case "en":
+		err = enTranslations.RegisterDefaultTranslations(validate, trans)
+	case "zh":
+		err = zhTranslations.RegisterDefaultTranslations(validate, trans)
+	default:
+		err = enTranslations.RegisterDefaultTranslations(validate, trans)
+	}
+	if err != nil {
+		panic("Failed to register translator when initializing validator")
+	}
+
+	err = 
 }
-
 
 func registerValidation() {
 	// 注册手机号验证规则
@@ -42,4 +86,43 @@ func registerValidation() {
 	if err != nil {
 		panic("Failed to register mobile validation")
 	}
+}
+
+
+type translation struct {
+	tag string
+	translation string
+	override bool
+	customRegisFunc validator.RegisterTranslationsFunc
+	customTransFunc validator.TranslationFunc
+}
+
+func customRegisTranslation() error {
+	translations := []translation{
+		{
+			tag: "mobile",
+			translation: "{0}格式不正确",
+			override: false,
+		},
+	}
+}
+
+
+func registerTranslation(translations []translation) (err error) {
+	for _, t := range translations {
+		if t.customTransFunc != nil && t.customRegisFunc != nil {
+			err = validate.RegisterTranslation(t.tag, trans, t.customRegisFunc, t.customTransFunc)
+		} else if t.customTransFunc != nil && t.customRegisFunc == nil {
+			err = validate.RegisterTranslation(t.tag, trans, registrationFunc(t.tag, t.translation, t.override), t.customTransFunc)
+		} else if t.customTransFunc == nil && t.customRegisFunc != nil {
+			err = validate.RegisterTranslation(t.tag, trans, t.customRegisFunc, translateFunc)
+		} else {
+			err = validate.RegisterTranslation(t.tag, trans, registrationFunc(t.tag, t.translation, t.override), translateFunc)
+		}
+
+		if err != nil {
+			return
+		}
+	}
+	return
 }
